c690120f8a78d4787a064f292adad4046de372b3
Tatyana Kopina <tatyanakopina@gmail.com>
2021-02-13 19:15:47 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int cost_ticket = 1_500_099;
+        int bonus = 2_000;
+        int mill = cost_ticket / bonus;
+        System.out.println(mill);
+
+    }
+}
